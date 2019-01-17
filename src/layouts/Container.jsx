dba37b240c6aef9a1ac046ff537a13319da3b0dd
Luce <lucy_liu@live.ca>
2019-01-17 17:18:25 +0800

    First commit

---
+++ src/layouts/Container.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'react-emotion';
+import PropTypes from 'prop-types';
+
+const Wrapper = styled.section`
+  text-align: ${props => (props.center ? 'center' : '')};
+  margin: auto;
+  padding: 3rem 1.5rem;
+  width: 60%;
+  @media (max-width: ${props => props.theme.breakpoints.m}) {
+    width: 90%;
+  }
+  @media (max-width: ${props => props.theme.breakpoints.s}) {
+    width: 95%;
+  }
+  max-width: ${props => props.theme.layout[props.type]};
+  height: 100%;
+  flex: 1;
+`;
+
+const Container = ({ children, type, className, center }) => (
+  <Wrapper className={className} type={type} center={center}>
+    {children}
+  </Wrapper>
+);
+
+export default Container;
+
+Container.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.array, PropTypes.node]).isRequired,
+  type: PropTypes.string,
+  className: PropTypes.string,
+  center: PropTypes.object,
+};
---
+++ src/layouts/index.jsx
@@ -0,0 +1,7 @@
+import Container from './Container';
+import Content from './Content';
+import Footer from './Footer';
+import Layout from './Layout';
+import NavBar from './NavBar';
+
+export { Container, Content, Footer, Layout, NavBar };
