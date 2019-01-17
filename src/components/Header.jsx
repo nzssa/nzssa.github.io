3635c8afd42e84eb2a75e0364672753fb3484d15
Luce <lucy_liu@live.ca>
2019-01-17 21:13:21 +0800

    added typeface Chivo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,6 @@ import Img from 'gatsby-image';
 import PropTypes from 'prop-types';
 
 const Wrapper = styled.header`
-  -webkit-clip-path: polygon(100% 0, 0 0, 0 70%, 50% 100%, 100% 70%);
-  clip-path: polygon(100% 0, 0 0, 0 70%, 50% 100%, 100% 70%);
-  @media (max-width: ${props => props.theme.breakpoints.s}) {
-    -webkit-clip-path: polygon(100% 0, 0 0, 0 90%, 50% 100%, 100% 90%);
-    clip-path: polygon(100% 0, 0 0, 0 90%, 50% 100%, 100% 90%);
-  }
   background: ${props => props.theme.gradient.rightToLeft};
   height: 300px;
   @media (max-width: ${props => props.theme.breakpoints.m}) {
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -24,8 +24,8 @@ const Index = ({ data }) => {
   const { edges } = data.allMarkdownRemark;
   return (
     <Layout>
-      <Helmet title={'Home Page'} />
-      <Header title="Home Page">Gatbsy Tutorial Starter</Header>
+      <Helmet title={"Home Page"} />
+      <Header title="New Zealand Students' Space Association">(mission statement)</Header>
       <PostWrapper>
         {edges.map(({ node }) => (
           <PostList
