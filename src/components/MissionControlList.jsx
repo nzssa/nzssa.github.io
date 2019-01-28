2675cfa84dea613325520160838141f4e144c2b1
Luce <lucy_liu@live.ca>
2019-01-29 00:08:32 +0800

    fixed TypeError (cannot load src from undefined)

--- src/components/MissionControlList.jsx
+++ src/components/MissionControlList.jsx
@@ -7,12 +7,11 @@ const Wrapper = styled.article`
   margin: 0 3rem;
 `;
 
-const MissionControlList = ({ id, cover, title }) => (
+const MissionControlList = ({ key, pic, title }) => (
   <Container>
     <Wrapper>
-      <p>Id is: {id}</p>
       <h1>{title}</h1>
-      <Img fluid={cover} />
+      <Img fluid={pic} />
     </Wrapper>
   </Container>
 );
