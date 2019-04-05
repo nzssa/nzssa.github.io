1f52e9d310af3d6b4807e586da5299b2d6f58b77
Amber Joseph <jamber919@gmail.com>
2019-04-05 20:18:56 +0800

    Updated site.js

--- config/site.js
+++ config/site.js
@@ -1,18 +1,18 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
   title: "New Zealand Students' Space Association", // Navigation and Site Title
-  titleAlt: 'Gatsby Tutorial Starter', // Title for JSONLD
-  description: 'A Gatsby V2 Starter Template Built with a Step By Step Guide',
-  url: 'https://gatsby-tutorial-starter.netlify.com', // Domain of your site. No trailing slash!
-  siteUrl: 'https://gatsby-tutorial-starter.netlify.com', // url + pathPrefix
+  titleAlt: 'NZSSA', // Title for JSONLD
+  description: 'For all students passionate about the space industry and its future',
+  url: 'https://nzssa.org.nz', // Domain of your site. No trailing slash!
+  siteUrl: 'https://nzssa.org.nz', // url + pathPrefix
   siteLanguage: 'en', // Language Tag on <html> element
   logo: 'static/logo/logo.png', // Used for SEO
   banner: 'static/logo/banner.png',
   // JSONLD / Manifest
   favicon: 'static/logo/favicon.png', // Used for manifest favicon generation
-  shortName: 'GatsbyTut', // shortname for manifest. MUST be shorter than 12 characters
-  author: 'Justin', // Author for schemaORGJSONLD
+  shortName: 'NZSSA', // shortname for manifest. MUST be shorter than 12 characters
+  author: '', // Author for schemaORGJSONLD
   themeColor: '#3e7bf2',
   backgroundColor: '#d3e0ff',
-  twitter: '@justinformentin', // Twitter Username
+  //twitter: '@justinformentin', // Twitter Username
 };
