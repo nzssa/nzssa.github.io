e79cd622e0d12278e68ac5e0b7ed71d4ea7c20da
Luce <lucy_liu@live.ca>
2019-06-02 14:11:08 -0400

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4cf1192c9d914149d28c.js"
+    "url": "webpack-runtime-2b83ca29430558022b8f.js"
   },
   {
     "url": "app-931151288efbb7b25e88.js"
@@ -36,20 +36,20 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "11a61c24e6bfaca24ab5ae348baf9038"
+    "revision": "3c1c48491fdbf9df305db7076d8118c2"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8aa9948b39c418aa8193aba390b1ca24"
+    "revision": "fec12ea664c145ddfccc8280b4d9d90a"
   },
   {
     "url": "styles.59f2b6d59b8604bd8574.css"
   },
   {
-    "url": "component---src-pages-index-jsx-800dcdf3f77c082b95c5.js"
+    "url": "component---src-pages-index-jsx-68ff0bd16f0a5fabda9d.js"
   },
   {
-    "url": "2-ecf8c90aa262dc1257aa.js"
+    "url": "2-da92997ddaa8fe08f600.js"
   },
   {
     "url": "1-6929de2cacad43408087.js"
@@ -58,8 +58,8 @@ self.__precacheManifest = [
     "url": "styles-29a512d1ac3c8680d953.js"
   },
   {
-    "url": "static/d/531/path---index-6a9-k1woy5v8Ntx8pEYkWS2F1kRiE.json",
-    "revision": "c173d74ba09d12dd22abc51003c63953"
+    "url": "static/d/57/path---index-6a9-5fieToVOamT6Ta4vVu7EWOtoWg.json",
+    "revision": "78d9e0474fe8d1d862a3f6d499e4d7a6"
   },
   {
     "url": "component---src-pages-404-jsx-419bd50a49c8200e32b4.js"
